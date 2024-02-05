ddc7505d3e38fee9ec0be94fc8ab8bd1950e7912
Edward Speer <espeer@caltech.edu>
2024-02-05 11:44:56 -0800

    add value checking on pwm output

--- pumpUtil/pump_util.py
+++ pumpUtil/pump_util.py
@@ -13,4 +13,6 @@ def send_pwm(value: int, device: Serial = PUMP) -> None:
     port. Defaults to /dev/ttyACM0, but can choose any port. (IF WE MOVE THE 
     PUMP USB, CHANGE PORT)
     """
+    if value < 0 or value > 255:
+        raise ValueError("PWM value must be between 0 and 255")
     device.write(f"{value}".encode("utf-8"))
