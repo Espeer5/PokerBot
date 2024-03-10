dfec1da103930fbf960f8a24dd32797ec6942c8e
leo <lbjenkin@caltech.edu>
2024-03-09 22:50:33 -0800

    Add filtering to ButtonDetector
    Waits for button to stop moving, then takes
    the average x and y.

--- detectors/detectors/ButtonDetector.py
+++ detectors/detectors/ButtonDetector.py
@@ -25,7 +25,6 @@ from detectors.utilities.mapping_utilities import pixel_to_world_2
 MIN_BUTTON_AREA = 250
 
 
-
 #
 #  Detector Node Class
 #
@@ -62,43 +61,54 @@ class ButtonDetectorNode(Detector):
             return response
         response.success = True
 
-        image = self.prev_images[-1]
-        # Ensure the previous image is able to be processed
-        assert(image.encoding == "rgb8")  # lies
+        # image = self.prev_images[-1]
+        xs = []
+        ys = []
+        for image in self.prev_images:
+            # Ensure the previous image is able to be processed
+            assert(image.encoding == "rgb8")  # lies
 
-        # Convert into OpenCV image, using RGB 8-bit (pass-through).
-        frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
+            # Convert into OpenCV image, using RGB 8-bit (pass-through).
+            frame = self.bridge.imgmsg_to_cv2(image, "bgr8")
 
-        hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-        binary = cv2.inRange(hsv, self.hsvlimits[:,0], self.hsvlimits[:,1])
-        # cv2.imshow("binary", binary)
-        # cv2.waitKey(0)
+            hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+            binary = cv2.inRange(hsv, self.hsvlimits[:,0], self.hsvlimits[:,1])
+            # cv2.imshow("binary", binary)
+            # cv2.waitKey(0)
 
-        contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            contours, _ = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # assert len(contours) == 1
-        for contour in contours:
-            (u, v), r = cv2.minEnclosingCircle(contour)
-            area = cv2.contourArea(contour) 
-            circle_area = np.pi * r**2
+            # assert len(contours) == 1
+            for contour in contours:
+                (u, v), r = cv2.minEnclosingCircle(contour)
+                area = cv2.contourArea(contour) 
+                circle_area = np.pi * r**2
 
-            if area >= MIN_BUTTON_AREA and area / circle_area >= 0.75:
-                # self.get_logger().info(f"{cv2.contourArea(contour)}")
+                if area >= MIN_BUTTON_AREA and area / circle_area >= 0.75:
+                    # self.get_logger().info(f"{cv2.contourArea(contour)}")
 
-                world_coords = pixel_to_world_2(frame, round(u), round(v))
+                    world_coords = pixel_to_world_2(frame, round(u), round(v))
 
-                if world_coords is not None:
-                    debugging_frame = frame.copy()
-                    cv2.drawContours(debugging_frame, [contour], -1, (255, 0, 0), 3)
-                    self.debugpub.publish(self.bridge.cv2_to_imgmsg(debugging_frame, "rgb8"))
-                    
-                    # cv2.imshow("debug", frame)
-                    # cv2.waitKey(0)
+                    if world_coords is not None:
+                        debugging_frame = frame.copy()
+                        cv2.drawContours(debugging_frame, [contour], -1, (255, 0, 0), 3)
+                        self.debugpub.publish(self.bridge.cv2_to_imgmsg(debugging_frame, "rgb8"))
+                        
+                        # cv2.imshow("debug", frame)
+                        # cv2.waitKey(0)
 
-                    x, y = world_coords
-                    point = f"{x}, {y}, {-0.02}"
-                    response.message = point
-    
+                        x, y = world_coords
+                        xs.append(x)
+                        ys.append(y)
+
+
+        avg_x = np.average(xs)
+        avg_y = np.average(ys)
+        avg_point = np.array([avg_x, avg_y]).reshape(3, 1)
+        last_point = np.array([xs[-1], ys[-1]]).reshape(3, 1)
+        if np.linalg.norm(avg_point - last_point) < 0.01:
+            point = f"{avg_x}, {avg_y}, {-0.02}"
+            response.message = point
         return response
 
 
